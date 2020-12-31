bd4651f0b4dfb8d34232b79c16da7d6d39a51e03
Simon Baumgardt-Wellander <simonbw@uw.edu>
2020-12-31 10:08:36 -0800

    baseball bat sounds

--- src/highrise/weapons/melee/melee-weapons/BaseballBat.ts
+++ src/highrise/weapons/melee/melee-weapons/BaseballBat.ts
@@ -1,7 +1,7 @@
-import snd_fleshHit3 from "../../../../../resources/audio/impacts/flesh-hit-3.flac";
-import snd_fleshHit4 from "../../../../../resources/audio/impacts/flesh-hit-4.flac";
 import snd_fleshHitBat1 from "../../../../../resources/audio/impacts/flesh-hit-bat-1.flac";
-import snd_swordShing2 from "../../../../../resources/audio/weapons/sword-shing-2.flac";
+import snd_fleshHitBat2 from "../../../../../resources/audio/impacts/flesh-hit-bat-2.flac";
+import snd_baseballBatPickup1 from "../../../../../resources/audio/weapons/baseball-bat-pickup-1.flac";
+import snd_baseballBatPickup2 from "../../../../../resources/audio/weapons/baseball-bat-pickup-2.flac";
 import snd_swordSwoosh1 from "../../../../../resources/audio/weapons/sword-swoosh-1.flac";
 import snd_swordSwoosh2 from "../../../../../resources/audio/weapons/sword-swoosh-2.flac";
 import snd_swordSwoosh3 from "../../../../../resources/audio/weapons/sword-swoosh-3.flac";
@@ -39,7 +39,7 @@ export const BaseballBat: MeleeWeaponStats = {
   sounds: {
     ...defaultMeleeWeapon.sounds,
     swing: [snd_swordSwoosh1, snd_swordSwoosh2, snd_swordSwoosh3],
-    hitFlesh: [snd_fleshHitBat1],
-    pickup: [snd_swordShing2],
+    hitFlesh: [snd_fleshHitBat1, snd_fleshHitBat2],
+    pickup: [snd_baseballBatPickup1, snd_baseballBatPickup2],
   },
 };
